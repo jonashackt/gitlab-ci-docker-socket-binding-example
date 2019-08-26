fe515101279d19be568efea5c24b62b1a6fb0055
Jonas Hecht <jonas.hecht@codecentric.de>
2019-08-26 15:13:08 +0200

    Refactored tests corresponding to port definition now.

--- src/test/java/de/jonashackt/restexamples/RestexamplesApplicationTests.java
+++ src/test/java/de/jonashackt/restexamples/RestexamplesApplicationTests.java
@@ -1,17 +1,17 @@
 package de.jonashackt.restexamples;
 
 import de.jonashackt.restexamples.controller.Controller;
+import io.restassured.RestAssured;
 import org.apache.http.HttpStatus;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
 
 import static io.restassured.RestAssured.get;
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(
@@ -19,28 +19,29 @@ import static org.junit.Assert.assertEquals;
 		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
 public class RestexamplesApplicationTests {
-    
+
+	@LocalServerPort
+	private int port;
+
+	@Before
+	public void init() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
 	@Test
 	public void testWithSpringRestTemplate() {
-	    // Given
-	    RestTemplate restTemplate = new RestTemplate();
-	    
-	    // When
-	    String response = restTemplate.getForObject("http://localhost:8080/restexamples/hello", String.class);
-	    
-	    // Then
-	    assertEquals(Controller.RESPONSE, response);
+		get("/restexamples/hello")
+		.then()
+			.statusCode(HttpStatus.SC_OK)
+			.assertThat()
+			.body(is(Controller.RESPONSE));
 	}
-	
-	/**
-	 * Using Restassured for elegant REST-Testing, see https://github.com/jayway/rest-assured
-	 */
+
 	@Test
     public void testWithRestAssured() {
-	    
-	    given() // can be ommited when GET only
-        .when() // can be ommited when GET only
-            .get("http://localhost:8080/restexamples/hello")
+
+		get("/restexamples/hello")
         .then()
             .statusCode(HttpStatus.SC_OK)
             .assertThat()
@@ -49,7 +50,7 @@ public class RestexamplesApplicationTests {
 
     @Test public void
 	check_if_responded_branch_name_is_correct() {
-		get("http://localhost:8080/restexamples/branchname")
+		get("/restexamples/branchname")
 		.then()
 			.statusCode(HttpStatus.SC_OK)
 			.equals(Controller.BRANCH_RESPONSE + "master");
